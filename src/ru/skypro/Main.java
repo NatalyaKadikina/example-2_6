e4b88a30478a52b53a77e645cb0dfddbbac13ee6
Наталья Кадикина <natalya92filipp@gmail.com>
2022-10-14 16:25:23 +0300

    Сделано

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,36 @@
 package ru.skypro;
 
+import java.util.*;
+
 public class Main {
+    private static final List<Integer> nums = new ArrayList<Integer>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    private static final List<String> words = new ArrayList<String>(List.of("abc", "abc", "bcd", "bcd", "e"));
 
     public static void main(String[] args) {
-	// write your code here
+
+//        //Задание 1
+        for (int num : nums) {
+            if (num % 2 != 0) {
+                System.out.println(num);
+            }
+        }
+
+        //Задание 2
+        Collections.sort(nums);
+        int prevNum = Integer.MIN_VALUE;
+        for (int num : nums) {
+            if (num % 2 == 0 && prevNum != num) {
+                System.out.println(num);
+                prevNum = num;
+            }
+        }
+
+        //Задание 3
+        Set<String> uniqueWords = new HashSet<>(words);
+        System.out.println(uniqueWords);
+
+        //Задание 4
+        Set<String> uniqueWords1 = new HashSet<>(words);
+        System.out.println(words.size() - uniqueWords.size());
     }
 }
